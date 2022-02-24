7ee3f69e09c0136876d31c4bab16848c5bbdf73f
Thymen Heuft <thymen.heuft@kpnmail.nl>
2022-02-24 13:56:05 +0000

    Appendix A: File Added

---
+++ 06_insertAssistants.sql
@@ -0,0 +1,13 @@
+USE SuperheroesDb
+
+INSERT INTO Assistant
+(Name, SuperheroId)
+VALUES('Happy', 1);
+
+INSERT INTO Assistant
+(Name, SuperheroId)
+VALUES('Alfred', 2);
+
+INSERT INTO Assistant
+(Name, SuperheroId)
+VALUES('Tyr-Van', 3);
