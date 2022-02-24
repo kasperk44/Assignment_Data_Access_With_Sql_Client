1999aed63d924861ef4179b078df71a10a2d011e
Thymen Heuft <thymen.heuft@kpnmail.nl>
2022-02-24 13:55:57 +0000

    Appendix A: File Added

---
+++ 05_insertSuperheroes.sql
@@ -0,0 +1,13 @@
+USE SuperheroesDb
+
+INSERT INTO Superhero
+(Name, Alias, Origin)
+VALUES('Peter Parker', 'Spiderman', 'USA');
+
+INSERT INTO Superhero
+(Name, Alias, Origin)
+VALUES('Bruce Wayne', 'Batman', 'USA');
+
+INSERT INTO Superhero
+(Name, Alias, Origin)
+VALUES('Clark Kent', 'Superman', 'USA');
